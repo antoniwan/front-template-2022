6e1844f044de59d4313d217955a2ecb8b74b5669
Antonio Rodriguez <antoniwan@gmail.com>
2022-07-02 15:23:06 -0400

    🐛 FIX: Type on GFonts inclusion

--- pages/_document.js
+++ pages/_document.js
@@ -27,7 +27,6 @@ export default class MyDocument extends Document {
       <Html lang="en-US">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
