2f63efe9653f155bf1e0d450b80ee6e74b5cff3e
Antonio Rodriguez <antoniwan@gmail.com>
2022-07-26 11:30:45 -0400

    Create SignIn.tsx

---
+++ components/SignIn.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Form = styled.form``;
+
+export default function SignIn() {
+  return (
+    <Form>
+      <input type="text" />
+    </Form>
+  );
+}
