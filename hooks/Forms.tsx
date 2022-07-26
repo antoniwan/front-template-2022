5e703beee3d16207d407644d1373155c37e2a5a4
Antonio Rodriguez <antoniwan@gmail.com>
2022-07-26 12:01:23 -0400

    Create Forms.tsx

---
+++ hooks/Forms.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+export default function useForm(initial = {}) {
+  // create a state object for our inputs
+  const [inputs, setInputs] = useState(initial);
+  const initialValues = Object.values(initial).join("");
+
+  useEffect(() => {
+    setInputs(initial);
+  }, [initialValues]);
+
+  function handleChange(e) {
+    let { value } = e.target;
+    const { name, type } = e.target;
+    if (type === "number") {
+      value = parseInt(value);
+    }
+    if (type === "file") {
+      [value] = e.target.files;
+    }
+    setInputs({
+      // copy the existing state
+      ...inputs,
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key]) => [key, ""])
+    );
+    setInputs(blankState);
+  }
+
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
+}
